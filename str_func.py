0a58902a16460ecd12d1ac679ba474644f0a8aed
ScrenOne <136308085+ScrenOne@users.noreply.github.com>
2024-04-12 15:45:23 +0400

    Update str_func.py
    Добавлен docstring к функции uppercase_string

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def uppercase_string(input_string):
+    """
+    Convert the input string to uppercase.
+
+    Args:
+        input_string (str): The string to be converted to uppercase.
+
+    Returns:
+        str: The input string converted to uppercase.
+    """
     return input_string.upper()
+
